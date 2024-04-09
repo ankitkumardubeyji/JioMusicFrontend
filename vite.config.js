7045c3a8f348efcc65bf209ed1183122a04b3241
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-04-09 19:09:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://jiomusicbackend.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => `/api${path}` // Add the '/api' prefix
+  server:{
+      proxy:{
+        '/api':'https://jiomusicbackend.onrender.com'
       }
-    }
   },
-  plugins: [react()]
-});
+  plugins: [react()],
+})
