31c131c96fc8ee2f9719f29f72bf558a3bfca09b
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-04-11 15:55:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://jiomusicbackend.onrender.com',
+      '/api':'http://jiomusicbackend.onrender.com',
     }
   },
   plugins: [react()],
