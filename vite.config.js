a1b95d01013d913d81cac1697dcbce4df532367a
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-04-15 17:56:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://jiomusicbackend.onrender.com',
+        target: 'https://jiomusicbackend.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
