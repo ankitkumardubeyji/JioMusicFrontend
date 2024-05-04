dc9b55a0be4f7f99006e5234fad84be08a5d9aa1
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-05-04 12:14:12 +0000

    updated UploadSong

--- src/componets/UploadSong.jsx
+++ src/componets/UploadSong.jsx
@@ -33,7 +33,7 @@ async function uploadNewVideo(event){
     console.log("edhar tak bhi aa gaya tha ")
     console.log(formData)
 
-    const response =  dispatch(publishSong(formData)).then(()=>dispatch(getSongs("")).then(()=>navigate("/")))
+    const response =  dispatch(publishSong(formData)).then(()=>dispatch(getSongs("")).then(()=>setTimeout(()=>navigate("/"),2000)))
     setUploadData({
         title:"",
         description:"",
