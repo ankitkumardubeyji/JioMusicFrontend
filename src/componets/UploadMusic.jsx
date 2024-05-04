e5d6f5dd19e65b67a91f0e0146338003bb403a42
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-05-04 12:44:35 +0000

    updated uploadMusic

--- src/componets/UploadMusic.jsx
+++ src/componets/UploadMusic.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 import { useDispatch } from "react-redux"
-import { publishSong } from "../Reducer/songSlice"
+import { getSongs, publishSong } from "../Reducer/songSlice"
 
 
 
