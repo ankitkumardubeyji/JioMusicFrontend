4b7f05aa32febebd420876e73ea458ac32dc0bb0
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-05-04 10:25:24 +0000

    update vite

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  server:{
-    proxy:{
-      '/api':'http://localhost:5456',
-    }
-  },
-=======
->>>>>>> 6920270f652d1f50a812a9619f81ae5a87c140d5
   plugins: [react()],
   server: {
     proxy: {
