1278277eeb5f5c779ee1d897e8b61008c790042b
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-05-05 06:57:10 +0000

    update search functionality

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -35,16 +35,13 @@ function Header(){
     }
 
     function handleSubmit(e){
-        let [location,setLocation] = useState("")
         e.preventDefault()
         console.log("here came for the submisson")
         console.log(searchValue)
         let searchQuery = `?query=${searchValue}`
-        dispatch(getArtistProfile(searchValue)).then(()=>setLocation("/music"))
-        dispatch(searchSongs(searchQuery)).then(()=>setLocation("/search"))
-        setTimeout(()=>navigate(location),3000)
-  
-
+        dispatch(getArtistProfile(searchValue)).then(()=>navigate("/music"))
+        dispatch(searchSongs(searchQuery)).then(()=>navigate("/search"))
+        setSearchValue("")
     
        
        
