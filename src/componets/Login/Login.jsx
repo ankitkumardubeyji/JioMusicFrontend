39d2919af6a18b72ba0400b64fa17127aaec0a1a
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-05-06 02:58:33 +0000

    updated login

--- src/componets/Login/Login.jsx
+++ src/componets/Login/Login.jsx
@@ -14,14 +14,15 @@ function Login() {
     username: "",
   });
 
-  async function validateAccount(e) {
+  function validateAccount(e) {
     e.preventDefault();
-  
+    console.log("came to frontend for logout");
+
     if (!signInData.email || !signInData.password || !signInData.username) {
       toast.error("Please fill in all the details");
       return;
     }
-  
+
     if (
       !signInData.email.match(
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -30,7 +31,7 @@ function Login() {
       toast.error("Invalid email address");
       return;
     }
-  
+
     if (
       !signInData.password.match(
         /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/
@@ -41,30 +42,18 @@ function Login() {
       );
       return;
     }
-  
-    try {
-      const response = await dispatch(validateUserAccount(signInData));
-  
-      if (response.payload.success) {
-        toast.success("Login successful");
-        setTimeout(() => {
-          navigate("/");
-        }, 2000);
-      } else {
-        toast.error(response.payload.error);
-      }
-  
-      setSignInData({
-        email: "",
-        password: "",
-        username: "",
-      });
-    } catch (error) {
-      console.error("Error while logging in:", error);
-      toast.error("An error occurred while logging in");
-    }
+
+    const response = dispatch(validateUserAccount(signInData)).then(()=>setTimeout(()=>navigate("/"),2000))
+
+    setSignInData({
+      email: "",
+      password: "",
+      username: "",
+    });
+
+   
   }
-  
+
   function handleUserInput(event) {
     const { name, value } = event.target;
     setSignInData({
